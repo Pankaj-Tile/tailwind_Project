e687d28ce061e70dc509bf74ab7c438ab9a5d2fb
Pankaj-Tile <pankajtile02@gmail.com>
2024-07-06 14:10:36 +0530

    creating custom classes in tailwind config file lec-11 completed

--- lec11/tailwind.config.js
+++ lec11/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    screens:{ 
+      // we cannot extend screens
+      xsm:'500px',
+      md:'768px'
+
+
+    },
+    extend: {
+      spacing:{
+        // creating our own tailwind code here
+      13: '300px'
+      },
+      fontSize:{
+        '10xl' :['9rem',{lineHeight:'1.2'}]
+      }
+    },
   },
   plugins: [],
 }
